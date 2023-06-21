a03f10bb5c7c25f46e813b85c95f630e217a4070
German Macias <germanmac89@gmail.com>
2023-06-21 11:12:56 -0700

    finished

---
+++ src/components/form.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Form = (props) => {
+  const [ firstName, setFirstName ] = useState("");
+  const [ lastName, setLastName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ confirmPassword, setConfirmPassword] = useState("");
+ }
+
+export default Form;
